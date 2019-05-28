e2a452d9fe2911190b644c251d589d87c7eaf76c
Tin <tinnystudios@gmail.com>
2019-05-28 21:46:43 +1000

    Add Stages

---
+++ Assets/_Project/Scripts/StageRunner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// A top level manager to run children stages
+/// </summary>
+public class StageRunner : MonoBehaviour
+{
+    private StageBase[] _stages;
+
+    public Coroutine Begin()
+    {
+        _stages = GetComponentsInChildren<StageBase>();
+        return StartCoroutine(Run());
+    }
+
+    public IEnumerator Run()
+    {
+        foreach (var stage in _stages)
+        {
+            yield return stage.Run();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/WaveStage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+public class WaveStage : StageBase
+{
+    public WaveModel Model;
+    public Spawner Spawner;
+
+    public override IEnumerator Run()
+    {
+        while (true)
+        {
+            var enemy = RandomUtils.GetByProbability(Model.Enemies).Enemy;
+            yield return Spawner.Spawn(enemy);
+        }
+    }
+}
