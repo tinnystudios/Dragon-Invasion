c142096ae0441e5bb2c8bcd3bd0837294be548e6
Tin <tinnystudios@gmail.com>
2019-05-28 21:47:02 +1000

    App data Binding

---
+++ Assets/_Project/Scripts/App.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// The root of the application
+/// </summary>
+public class App : MonoBehaviour, IBind<StageRunner>
+{
+    private StageRunner _stageRunner;
+    public void Bind(StageRunner dependent) => _stageRunner = dependent;
+
+    /// <summary>
+    /// The end of this coroutine signifies the end of the application
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Start()
+    {
+        var dataBinder = gameObject.AddComponent<AppDataBinder>();
+        dataBinder.Bind();
+
+        yield return _stageRunner.Begin();
+    }
+}
