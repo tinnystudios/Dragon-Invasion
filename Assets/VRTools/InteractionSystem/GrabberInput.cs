8270d5ff022b47e5c6ad551381e5b67dfde3d460
Tin <tin@liminalvr.com>
2019-05-30 16:09:37 +1000

    Add Trigger Support

--- Assets/VRTools/InteractionSystem/GrabberInput.cs
+++ Assets/VRTools/InteractionSystem/GrabberInput.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class GrabberInput
 {
-    // LTouch
-
     public Action OnGrabDown;
     public Action OnGrabUp;
     public Action OnGrab;
@@ -15,10 +13,13 @@ public class GrabberInput
         {
             case EHandedness.Left:
                 return Input.GetKey(KeyCode.G) ||
-                    OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) != 0;
+                    OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) != 0 ||
+                    OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) != 0;
+
             case EHandedness.Right:
                 return Input.GetKey(KeyCode.B) ||
-                    OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) != 0;
+                    OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) != 0 ||
+                    OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) != 0;
             default:
                 return false;
         }
@@ -31,20 +32,21 @@ public class GrabberInput
         switch (hand)
         {
             case EHandedness.Left:
-                state = 
+                state =
                     Input.GetKeyUp(KeyCode.G) ||
-                    OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger);
+                    OVRInput.GetUp(OVRInput.Button.PrimaryHandTrigger) ||
+                    OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
                 break;
             case EHandedness.Right:
                 state = 
                     Input.GetKeyUp(KeyCode.B) ||
-                    OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger);
+                    OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger) ||
+                    OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger);
                 break;
         }
 
         if (state)
         {
-            Debug.Log("Grab up");
             OnGrabUp?.Invoke();
         }
 
@@ -58,5 +60,3 @@ public enum EHandedness
     Right,
     All
 }
-
-
