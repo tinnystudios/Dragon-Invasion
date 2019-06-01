675d04a1a555fbcf8ea7eac2c182cd58a183fea9
Tin <tinnystudios@gmail.com>
2019-06-01 17:14:53 +1000

    Add a fireball as a sphere

---
+++ Assets/_Project/Scripts/CoolDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoolDown : MonoBehaviour
+{
+    public float MinDuration = 0.5F;
+    public float MaxDuration = 2.0F;
+
+    public bool IsCoolingDown { get; private set; }
+    
+    public Coroutine Begin()
+    {
+        return StartCoroutine(Run());
+    }
+
+    private IEnumerator Run()
+    {
+        var duration = Random.Range(MinDuration, MaxDuration);
+
+        IsCoolingDown = true;
+        yield return new WaitForSeconds(duration);
+        IsCoolingDown = false;
+    }
+}
---
+++ Assets/_Project/Scripts/FireballAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FireballAttack : EnemyAttackBase
+{
+    public Fireball FireballPrefab;
+    public Transform Pivot;
+    public CoolDown CoolDown;
+
+    public float Power = 3;
+
+    public override void Attack()
+    {
+        if (CoolDown.IsCoolingDown)
+            return;
+
+        var fireball = Instantiate(FireballPrefab, Pivot.position, Pivot.rotation);
+        var velocity = Pivot.forward * Power;
+
+        fireball.Fire(velocity);
+
+        CoolDown.Begin();
+    }
+}
