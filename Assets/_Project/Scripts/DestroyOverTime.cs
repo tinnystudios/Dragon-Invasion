6df2f3b6d5f58fc328781c973ec39cecfe71860d
Tin <tinnystudios@gmail.com>
2019-06-02 00:51:18 +1000

    Add Trees back in

---
+++ Assets/_Project/Scripts/DestroyOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    private float Lifetime = 2.0F;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
