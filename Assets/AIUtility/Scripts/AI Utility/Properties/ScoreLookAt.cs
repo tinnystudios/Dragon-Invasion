e6f1c30642e6e9737fcecc78ace8d95f29f15553
Tin <tinnystudios@gmail.com>
2019-06-02 20:22:04 +1000

    Add dodgable dragon

---
+++ Assets/AIUtility/Scripts/AI Utility/Properties/ScoreLookAt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// When the looker look near the agent, the score increases
+/// </summary>
+public class ScoreLookAt : ScorePropertyBase
+{
+    public Transform Looker;
+
+    public float Min = 0.9F;
+    public float Max = 1.0F;
+
+    public float MyScore;
+
+    public override float Score(DecisionContext context)
+    {
+        var dir = context.Intelligence.transform.position - Looker.transform.position;
+        dir.Normalize();
+
+        var dotProductToArrow = Vector3.Dot(dir, Looker.transform.forward);
+        MyScore = Mathf.InverseLerp(Min, Max, dotProductToArrow);
+
+        return Mathf.InverseLerp(Min, Max, dotProductToArrow);
+    }
+}
