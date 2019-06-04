c2648480dac5acd4c8f723d69b6d81ccbf0a769b
Tin <tin@liminalvr.com>
2019-06-04 14:36:28 +1000

    Fix Player Position

---
+++ Assets/VRHeadAlias.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHeadAlias : MonoBehaviour
+{
+    public Transform Head;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Head.position;
+    }
+}
